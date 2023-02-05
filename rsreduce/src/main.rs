4155461bb67acd1f0d4b8e0615315f3c31ad5885
Léo Gaspard <leo@gaspard.io>
2023-02-05 18:26:48 +0100

    Fill in the API a bit

--- rsreduce/src/main.rs
+++ rsreduce/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+use tree_sitter_reduce::ShellTest;
+
+#[derive(Debug, structopt::StructOpt)]
+struct Opt {
+    /// Path to the root of the crate (or workspace)
+    ///
+    /// The interestingness test will be run in a copy this folder.
+    root_path: PathBuf,
+
+    /// If this option is passed, then only the file passed to it will be reduced
+    ///
+    /// Pass multiple times to reduce only a specific list of files in the root path.
+    /// Paths are relative to the root path, by default all `.rs` files in the root
+    /// path will be reduced
+    // TODO: also support reducing the toml files, to remove external deps
+    #[structopt(long = "file")]
+    only_files: Option<Vec<PathBuf>>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let opt = Opt::from_args();
+    let test = ShellTest;
+    // TODO: remove unwrap below
+    tree_sitter_reduce::run(opt.root_path, opt.only_files.unwrap(), test, &[])
 }
--- tree-sitter-reduce/src/lib.rs
+++ tree-sitter-reduce/src/lib.rs
@@ -1,16 +1,57 @@
-use std::path::{PathBuf, Path};
+use std::path::{Path, PathBuf};
 
 pub trait Pass {
+    /// Prepare the root path for this pass
+    ///
+    /// This will be called with as argument the `root` passed to `run`. It should
+    /// make anything necessary to prepare the execution of the test on this pass.
+    /// For instance, for rust tests it can mean copying the `target` directory if
+    /// the `Cargo.toml` file was edited, so that it's possible to recover the
+    /// incremental compilation situation if the test was not actually interesting.
+    fn prepare(&self, root: &Path) -> anyhow::Result<()> {
+        let _ = root;
+        Ok(())
+    }
+
     /// Edit the file at `path`, simplifying it
     ///
     /// Note that for proper operation this MUST BE DETERMINISTIC! For this reason, a
     /// `random_seed` argument is provided, which the pass can use to initialize an RNG.
-    fn run(&self, path: &Path, random_seed: u64);
+    fn reduce(&self, path: &Path, random_seed: u64) -> anyhow::Result<()>;
+
+    /// Cleanup the root path after this pass' test ran
+    ///
+    /// `root` is the path passed to `run`. `was_interesting` tells this function
+    /// whether the run was actually interesting. For instance, a pass reducing
+    /// `Cargo.toml` dependencies could use it to determine which of the `target`
+    /// directories to keep.
+    fn cleanup(&self, root: &Path, was_interesting: bool) -> anyhow::Result<()> {
+        let _ = (root, was_interesting);
+        Ok(())
+    }
+}
+
+pub trait Test {
+    /// Run the test
+    ///
+    /// Returns `Err` in case an error made it impossible to know whether the current
+    /// state is interesting. Returns `Ok(true)` if the current state is interesting,
+    /// and `Ok(false)` if not.
+    fn test_interesting(&self, root: &Path) -> anyhow::Result<bool>;
+}
+
+pub struct ShellTest;
+
+impl Test for ShellTest {
+    fn test_interesting(&self, _root: &Path) -> anyhow::Result<bool> {
+        todo!()
+    }
 }
 
 pub fn run(
+    _root: PathBuf,
     _files: Vec<PathBuf>,
-    _cleanup: impl FnMut() -> anyhow::Result<()>,
+    _test: impl Test,
     _passes: &[&dyn Pass],
 ) -> anyhow::Result<()> {
     todo!()

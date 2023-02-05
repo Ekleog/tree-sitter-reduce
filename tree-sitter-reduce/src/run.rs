49660fe97370cda63f5a083a4de867c9b36aafeb
Léo Gaspard <leo@gaspard.io>
2023-02-05 19:42:45 +0100

    Copy the source to the worker directory

--- tree-sitter-reduce/src/run.rs
+++ tree-sitter-reduce/src/run.rs
@@ -1,12 +1,16 @@
 use std::path::{Path, PathBuf};
 
+use anyhow::Context;
+
 use crate::{Pass, Test};
 
 #[derive(Debug, structopt::StructOpt)]
 pub struct Opt {
     /// Path to the root of the crate (or workspace)
     ///
-    /// The interestingness test will be run in a copy this folder.
+    /// The interestingness test will be run in a copy this folder. Note that copies
+    /// can happen during the whole runtime of this program, so the folder should not
+    /// be changed during that time.
     root_path: PathBuf,
 
     /// If this option is passed, then only the file passed to it will be reduced
@@ -24,6 +28,13 @@ pub struct Opt {
 }
 
 impl Opt {
+    pub fn canonicalize_root_path(&mut self) -> anyhow::Result<()> {
+        Ok(self.root_path = self
+            .root_path
+            .canonicalize()
+            .with_context(|| "canonicalizing root path {root:?}")?)
+    }
+
     pub fn files(
         &self,
         default_list: impl Fn(&Path) -> anyhow::Result<Vec<PathBuf>>,
@@ -36,23 +47,35 @@ impl Opt {
 }
 
 pub fn run(
-    opt: Opt,
+    mut opt: Opt,
     filelist: impl Fn(&Path) -> anyhow::Result<Vec<PathBuf>>,
     _test: impl Test,
     _passes: &[&dyn Pass],
 ) -> anyhow::Result<()> {
+    opt.canonicalize_root_path()?;
     let _files = opt.files(filelist)?;
     let mut workers = Vec::new();
     for _ in 0..opt.jobs {
-        workers.push(Worker::new());
+        workers.push(Worker::new(&opt.root_path).context("spinning up worker")?);
     }
-    Ok(())
+    todo!()
 }
 
 struct Worker {}
 
 impl Worker {
-    fn new() -> Self {
+    fn new(root: &Path) -> anyhow::Result<Self> {
+        // First, copy the target into a directory
+        let dir = tempfile::Builder::new()
+            .prefix("tree-sitter-reduce")
+            .tempdir()
+            .context("creating temporary directory")?;
+        fs_extra::dir::copy(
+            root,
+            &dir,
+            &fs_extra::dir::CopyOptions::default().content_only(true),
+        )
+        .with_context(|| format!("copying source from {root:?} to {:?}", dir.path()))?;
         todo!()
     }
 }
